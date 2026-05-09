90cc1b7e741bc6f31418578685a27046d387ec92
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2026-05-09 13:11:08 +0530

    Update API proxy destination to production URL

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*', // Proxy to Backend
+        destination: 'https://privateproject-r0ry.onrender.com/api/:path*', // Proxy to Backend
       },
     ];
   },
